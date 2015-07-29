a10652f3869699db246f144dbcceb022676e9a68
Chesley Brown <chezbrown@gmail.com>
2015-07-29 11:24:54 -0230

    Fixes #5
    - adds ability to configure uservoice widget src

--- src/service.uservoice.js
+++ src/service.uservoice.js
@@ -2,13 +2,28 @@
 
 angular
 	.module('service.uservoice', [])
-	.factory('UserVoice', function ($window) {
+	.provider('$userVoice', function () {
+		var defaults = this.defaults = {
+			src: null
+		};
+		
+		this.$get = function () {
+			return {defaults: defaults};
+		};
+	})
+	.factory('UserVoice', function ($userVoice, $window) {
+		var options = $userVoice.defaults;
 		$window.UserVoice = $window.UserVoice || [];
+		
+		// Must set a uservoice src
+		if (!options.src) {
+			throw new Error('Missing required UserVoice src setting. Set using $userVoiceProvider.defaults.src');
+		}
 		
 		// Inject UserVoice script into DOM
 		// jscs:disable
 		/* jshint ignore:start */
-		(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/rnrmZDmdueiXkRibsCROg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+		(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src=options.src;var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
 		/* jshint ignore:end */
 		// jscs:enable
 		
