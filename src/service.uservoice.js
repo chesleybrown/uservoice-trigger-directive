230290f7192e0ca1a53689b081051e0cdac4ab47
Chesley Brown <chezbrown@gmail.com>
2015-07-29 11:35:43 -0230

    Converted to only need the javascript API key

--- src/service.uservoice.js
+++ src/service.uservoice.js
@@ -4,7 +4,7 @@ angular
 	.module('service.uservoice', [])
 	.provider('$userVoice', function () {
 		var defaults = this.defaults = {
-			src: null
+			key: null
 		};
 		
 		this.$get = function () {
@@ -15,15 +15,15 @@ angular
 		var options = $userVoice.defaults;
 		$window.UserVoice = $window.UserVoice || [];
 		
-		// Must set a uservoice src
-		if (!options.src) {
-			throw new Error('Missing required UserVoice src setting. Set using $userVoiceProvider.defaults.src');
+		// Must set a uservoice key
+		if (!options.key) {
+			throw new Error('Missing required UserVoice Javascript API key. Set using $userVoiceProvider.defaults.key');
 		}
 		
 		// Inject UserVoice script into DOM
 		// jscs:disable
 		/* jshint ignore:start */
-		(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src=options.src;var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+		(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/' + options.key + '.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
 		/* jshint ignore:end */
 		// jscs:enable
 		
